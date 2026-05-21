ae31f951fbd4a3ec9e29bb29d643f61751c2d277
jayasuryaNew <https://github.com/jayasuryaNew/Netflix.git>
2026-05-21 10:35:09 +0530

    Fix homepage product cache

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,21 +1,14 @@
+export const revalidate = 60;
+
 import HeroSection from "@/components/website/home/hero";
-
 import FeaturedProducts from "@/components/website/home/featured-products";
-
 import BrandStory from "@/components/website/home/brand-story";
-
 import ArchitecturalShowcase from "@/components/website/sections/architectural-showcase";
-
 import LuxuryCollections from "@/components/website/sections/luxury-collections";
-
 import ProjectShowcase from "@/components/website/sections/project-showcase";
-
 import InspirationGallery from "@/components/website/sections/inspiration-gallery";
-
 import DealerCTA from "@/components/website/sections/dealer-cta";
-
 import Testimonials from "@/components/website/sections/testimonials";
-
 import Newsletter from "@/components/website/sections/newsletter";
 
 export default function HomePage() {
